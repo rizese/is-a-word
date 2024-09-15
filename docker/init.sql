a8e457cfe6f1f65233aab20d35a532b12fdd0c71
Ricky Franzese <ricky+zese@thirtymadison.com>
2024-09-15 15:32:59 -0400

    database set up

--- docker/init.sql
+++ docker/init.sql
@@ -1,5 +1,26 @@
+-- Create grade enum type if it does not exist
+DO $$ BEGIN
+    CREATE TYPE grade_enum AS ENUM (
+        'PREK',
+        'KNDGTN',
+        'GRADE1',
+        'GRADE2',
+        'GRADE3',
+        'GRADE4',
+        'GRADE5',
+        'GRADE6',
+        'JRHIGHSCH',
+        'HIGHSCH',
+        'ADULT'
+    );
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
+-- Create words table if it does not exist
 CREATE TABLE IF NOT EXISTS words (
     id SERIAL PRIMARY KEY,
     word TEXT NOT NULL,
-    first_letter CHAR(1)
+    first_letter CHAR(1),
+    grade_level grade_enum
 );
