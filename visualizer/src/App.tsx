633faf48e1111d0e4e8f21d54521502be06fb78b
Ricky Franzese <ricky+zese@thirtymadison.com>
2024-09-17 22:49:36 -0400

    reactflow darkmode :)

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import ColorModeFlow from './components/ColorModeFlow';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -28,6 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <ColorModeFlow />
+      </div>
     </>
   );
 }
